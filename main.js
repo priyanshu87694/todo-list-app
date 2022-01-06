b0d3f4ac84beb1723ec5bc6f8cc760d20ad1863e
Priyanshu Jha <73730939+priyanshu87694@users.noreply.github.com>
2022-01-06 13:26:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,26 +16,29 @@ submit.onclick = function () {
     }
 };
 
-for (let i=0; i<localStorage.length; i++) {
+for (let i = 0; i < localStorage.length; i++) {
     const _key = localStorage.key(i);
     const _value = localStorage.getItem(_key);
 
     tasks.innerHTML += `${_key} >> ${_value} </br>`;
 
-
     const btn_div = document.createElement("div");
     btn_div.classList.add("btn_div");
+    tasks.appendChild(btn_div);
+}
+
+for (let i = 0; i < localStorage.length; i++) {
+    const _key = localStorage.key(i);
+    const _value = localStorage.getItem(_key);
 
     const del_btn = document.createElement("button");
     del_btn.classList.add("delete" + JSON.stringify(i));
     del_btn.innerText = "completed";
-    btn_div.appendChild(del_btn);
+    const btn_div = tasks.children[2*i + 1]
+    btn_div.appendChild(del_btn)
 
-    // delete button on click
     del_btn.onclick = function () {
         localStorage.removeItem(_key, _value);
         location.reload();
     }
-
-    tasks.appendChild(btn_div);
 }
