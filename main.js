461a08e1c661806d68723009f4f05f86084bd6a4
Priyanshu Jha <73730939+priyanshu87694@users.noreply.github.com>
2022-01-06 12:08:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const key = document.getElementById("key");
+const value = document.getElementById("value");
+const submit = document.getElementById("submit");
+const tasks = document.getElementById("tasks");
+
+submit.onclick = function () {
+    const key_txt = key.value;
+    const value_txt = value.value;
+
+    console.log(key_txt);
+    console.log(value_txt);
+
+    if (key && value) {
+        localStorage.setItem(key_txt, value_txt);
+        location.reload();
+    }
+};
+
+for (let i=0; i<localStorage.length; i++) {
+    const _key = localStorage.key(i);
+    const _value = localStorage.getItem(_key);
+
+    tasks.innerHTML += `${_key} >> ${_value} </br>`;
+
+
+    const btn_div = document.createElement("div");
+    btn_div.classList.add("btn_div");
+
+    const del_btn = document.createElement("button");
+    del_btn.classList.add("delete" + JSON.stringify(i));
+    del_btn.innerText = "completed";
+    btn_div.appendChild(del_btn);
+
+    // delete button on click
+    del_btn.onclick = function () {
+        localStorage.removeItem(_key, _value);
+        location.reload();
+    }
+
+    tasks.appendChild(btn_div);
+}
