3b537006f1f80a29c502a59f9346de75a5d4bf0a
Priyanshu Jha <73730939+priyanshu87694@users.noreply.github.com>
2022-01-06 15:06:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-const key = document.getElementById("key");
-const value = document.getElementById("value");
 const submit = document.getElementById("submit");
 const tasks = document.getElementById("tasks");
 
 submit.onclick = function () {
-    const key_txt = key.value;
-    const value_txt = value.value;
-
-    console.log(key_txt);
-    console.log(value_txt);
+    const key = document.getElementById("key").value;
+    const value = document.getElementById("value").value;
 
     if (key && value) {
-        localStorage.setItem(key_txt, value_txt);
+        localStorage.setItem(key, value);
         location.reload();
+    } else {
+        alert("Empty field/s not allowed");
     }
-};
+}
 
 for (let i = 0; i < localStorage.length; i++) {
     const _key = localStorage.key(i);
@@ -24,21 +21,19 @@ for (let i = 0; i < localStorage.length; i++) {
 
     const btn_div = document.createElement("div");
     btn_div.classList.add("btn_div");
+
     tasks.appendChild(btn_div);
 }
 
 for (let i = 0; i < localStorage.length; i++) {
-    const _key = localStorage.key(i);
-    const _value = localStorage.getItem(_key);
-
     const del_btn = document.createElement("button");
     del_btn.classList.add("delete" + JSON.stringify(i));
     del_btn.innerText = "completed";
-    const btn_div = tasks.children[2*i + 1]
-    btn_div.appendChild(del_btn)
+    tasks.children[2 * i + 1].appendChild(del_btn);
 
     del_btn.onclick = function () {
-        localStorage.removeItem(_key, _value);
+        const _key = localStorage.key(i);
+        localStorage.removeItem(_key);
         location.reload();
     }
 }
